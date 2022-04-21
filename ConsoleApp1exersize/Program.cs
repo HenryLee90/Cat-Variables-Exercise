273bf03566adf954db09454f462664f135e9df17
Henry Lee <hlee09151@gmail.com>
2022-04-21 14:53:17 -0500

    init

---
+++ ConsoleApp1exersize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1exersize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Henry");
+            int first = 90;
+            char second = (char)15;
+            bool cran =  true;
+            double catFood= 50.1;
+            decimal lives = 9M;
+            Console.WriteLine(lives);
+
+
+            string catName = "Yoshi";
+            int catAge = 1;
+
+            Console.WriteLine($"My cats name is {catName}, He is {catAge} years old, He eats {catFood} pounds of food a day, He has {lives} lives");
+            
+
+        }
+    }
+}
